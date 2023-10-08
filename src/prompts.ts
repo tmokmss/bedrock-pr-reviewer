911938e9cc850cdee755a35dbc186f0c0935071a
tmokmss <tmokmss@users.noreply.github.com>
2023-10-09 00:52:30 +0900

    change prompt

--- src/prompts.ts
+++ src/prompts.ts
@@ -4,75 +4,58 @@ export class Prompts {
   summarize: string
   summarizeReleaseNotes: string
 
-  summarizeFileDiff = `## GitHub PR Title
+  summarizeFileDiff = `
+I would like you to succinctly summarize the Pull Request within 100 words.
+The Pull Request is described with <title>, <description>, and <diff> tags.
+If applicable, your summary should include a note about alterations to the signatures of exported functions, global data structures and variables, and any changes that might affect the external interface or behavior of the code.
 
-\`$title\` 
+<title>
+$title 
+</title>
 
-## Description
-
-\`\`\`
+<description>
 $description
-\`\`\`
+</description>
 
-## Diff
-
-\`\`\`diff
+<diff>
 $file_diff
-\`\`\`
-
-## Instructions
-
-I would like you to succinctly summarize the diff within 100 words.
-If applicable, your summary should include a note about alterations 
-to the signatures of exported functions, global data structures and 
-variables, and any changes that might affect the external interface or 
-behavior of the code.
+</diff>
 `
-  triageFileDiff = `Below the summary, I would also like you to triage the diff as \`NEEDS_REVIEW\` or 
-\`APPROVED\` based on the following criteria:
+  triageFileDiff = `Below the summary, I would also like you to triage the diff as \`NEEDS_REVIEW\` or \`APPROVED\` based on the following criteria:
 
-- If the diff involves any modifications to the logic or functionality, even if they 
-  seem minor, triage it as \`NEEDS_REVIEW\`. This includes changes to control structures, 
-  function calls, or variable assignments that might impact the behavior of the code.
-- If the diff only contains very minor changes that don't affect the code logic, such as 
-  fixing typos, formatting, or renaming variables for clarity, triage it as \`APPROVED\`.
+- If the diff involves any modifications to the logic or functionality, even if they seem minor, triage it as \`NEEDS_REVIEW\`. This includes changes to control structures, function calls, or variable assignments that might impact the behavior of the code.
+- If the diff only contains very minor changes that don't affect the code logic, such as fixing typos, formatting, or renaming variables for clarity, triage it as \`APPROVED\`.
 
-Please evaluate the diff thoroughly and take into account factors such as the number of 
-lines changed, the potential impact on the overall system, and the likelihood of 
-introducing new bugs or security vulnerabilities. 
+Please evaluate the diff thoroughly and take into account factors such as the number of lines changed, the potential impact on the overall system, and the likelihood of introducing new bugs or security vulnerabilities. 
 When in doubt, always err on the side of caution and triage the diff as \`NEEDS_REVIEW\`.
 
 You must strictly follow the format below for triaging the diff:
 [TRIAGE]: <NEEDS_REVIEW or APPROVED>
 
 Important:
-- In your summary do not mention that the file needs a through review or caution about
-  potential issues.
+- In your summary do not mention that the file needs a through review or caution about potential issues.
 - Do not provide any reasoning why you triaged the diff as \`NEEDS_REVIEW\` or \`APPROVED\`.
-- Do not mention that these changes affect the logic or functionality of the code in 
-  the summary. You must only use the triage status format above to indicate that.
+- Do not mention that these changes affect the logic or functionality of the code in the summary. You must only use the triage status format above to indicate that.
 `
-  summarizeChangesets = `Provided below are changesets in this pull request. Changesets 
-are in chronlogical order and new changesets are appended to the
-end of the list. The format consists of filename(s) and the summary 
+  summarizeChangesets = `Provided below (<changeSet> tag) are changesets in this pull request.
+Changesets are in chronlogical order and new changesets are appended to the end of the list. The format consists of filename(s) and the summary 
 of changes for those files. There is a separator between each changeset.
-Your task is to deduplicate and group together files with
-related/similar changes into a single changeset. Respond with the updated 
-changesets using the same format as the input. 
+Your task is to deduplicate and group together files with related/similar changes into a single changeset. Respond with the updated changesets using the same format as the input. 
 
+<changeSet>
 $raw_summary
+</changeSet>
 `
 
-  summarizePrefix = `Here is the summary of changes you have generated for files:
-      \`\`\`
-      $raw_summary
-      \`\`\`
+  summarizePrefix = `Below <summary> tag is the summary of changes you have generated for files:
+<summary>
+$raw_summary
+</summary>
 
 `
 
-  summarizeShort = `Your task is to provide a concise summary of the changes. This 
-summary will be used as a prompt while reviewing each file and must be very clear for 
-the AI bot to understand. 
+  summarizeShort = `Your task is to provide a concise summary of the changes.
+This summary will be used as a prompt while reviewing each file and must be very clear for the AI bot to understand. 
 
 Instructions:
 
@@ -83,26 +66,23 @@ Instructions:
 - The summary should not exceed 500 words.
 `
 
-  reviewFileDiff = `## GitHub PR Title
+  reviewFileDiff = `
+<title>
+$title 
+</title>
 
-\`$title\` 
-
-## Description
-
-\`\`\`
+<description>
 $description
-\`\`\`
+</description>
 
-## Summary of changes
-
-\`\`\`
+<changes>
 $short_summary
-\`\`\`
+</changes>
 
 ## IMPORTANT Instructions
 
 Input: New hunks annotated with line numbers and old hunks (replaced code). Hunks represent incomplete code fragments.
-Additional Context: PR title, description, summaries and comment chains.
+Additional Context: <title>, <description>, <changes> and comment chains.
 Task: Review new hunks for substantive issues using provided context and respond with comments if necessary.
 Output: Review comments in markdown with exact line number ranges in new hunks. Start and end line numbers must be within the same hunk. For single-line comments, start=end line number. Must use example response format below.
 Use fenced code blocks using the relevant language identifier where applicable.
@@ -110,21 +90,16 @@ Don't annotate code snippets with line numbers. Format and indent code correctly
 Do not use \`suggestion\` code blocks.
 For fixes, use \`diff\` code blocks, marking changes with \`+\` or \`-\`. The line number range for comments with fix snippets must exactly match the range to replace in the new hunk.
 
-- Do NOT provide general feedback, summaries, explanations of changes, or praises 
-  for making good additions. 
-- Focus solely on offering specific, objective insights based on the 
-  given context and refrain from making broad comments about potential impacts on 
-  the system or question intentions behind the changes.
+- Do NOT provide general feedback, summaries, explanations of changes, or praises for making good additions. 
+- Focus solely on offering specific, objective insights based on the given context and refrain from making broad comments about potential impacts on the system or question intentions behind the changes.
 
-If there are no issues found on a line range, you MUST respond with the 
-text \`LGTM!\` for that line range in the review section. 
+If there are no issues found on a line range, you MUST respond with the text \`LGTM!\` for that line range in the review section. 
 
-## Example
-
+<example>
 ### Example changes
 
 ---new_hunk---
-\`\`\`
+<code>
   z = x / y
     return z
 
@@ -137,10 +112,10 @@ text \`LGTM!\` for that line range in the review section.
 
 def subtract(x, y):
   z = x - y
-\`\`\`
+</code>
   
 ---old_hunk---
-\`\`\`
+<code>
   z = x / y
     return z
 
@@ -149,7 +124,7 @@ def add(x, y):
 
 def subtract(x, y):
     z = x - y
-\`\`\`
+</code>
 
 ---comment_chains---
 \`\`\`
@@ -162,14 +137,15 @@ Please review this change.
 
 22-22:
 There's a syntax error in the add function.
-\`\`\`diff
+<diff>
 -    retrn z
 +    return z
-\`\`\`
+</diff>
 ---
 24-25:
 LGTM!
 ---
+</example>
 
 ## Changes made to \`$filename\` for your review
 
